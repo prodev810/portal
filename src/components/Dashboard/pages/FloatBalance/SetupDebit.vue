7db8e16e77557a14ae97ad9d09aea5869acf4da6
Dmitrii Alferev <dmitry@alferev.com>
2019-09-20 12:57:19 +0300

    New mockup wired with data, unused old code commented

--- src/components/Dashboard/pages/FloatBalance/SetupDebit.vue
+++ src/components/Dashboard/pages/FloatBalance/SetupDebit.vue
@@ -2,8 +2,8 @@
   <div>
     <div>
       <div>
-        <div class="card-header bg-white border-0">
-          <h4 class="card-title">
+        <div class="card-header bg-white border-0 pt-0">
+          <h4 class="card-title mt-0">
             {{ $t('setup_debit.create.title') }}
           </h4>
           <div class="pl-2">
@@ -17,6 +17,7 @@
             </ul>
           </div>
         </div>
+				<!--
         <div class="card">
           <div class="card-content row">
             <div class="col-sm-12">
@@ -44,17 +45,16 @@
             </div>
           </div>
         </div>
+				-->
       </div>
-      <sof-uploader
-        v-model="showSoftUploader"
-        :soft-docs="softDocs"
-        :select-only="true"
-        @dataChanged="void 0"
-        floatId="new_record"
-        @sofDocsSelected="handleSofDocsSelection"
-      >
-      </sof-uploader>
     </div>
+
+		<sof-uploader	v-model="showSoftUploader"
+									:soft-docs="softDocs"
+									:select-only="true"
+									@dataChanged="void 0"
+									floatId="new_record"
+									@sofDocsSelected="handleSofDocsSelection"/>
 
     <!-- New style mock start-->
     <div class="row">
@@ -62,31 +62,42 @@
         <div class="card ceevo__card-group">
           <div class="row card-content p-4">
             <!-- left content-->
-            <div class="w-50 w-sm-100 pl-4 pr-4 pr-md-3">
+            <div class="w-50 w-sm-100 pl-4 pr-4 pr-md-3 pr-lg-4 pr-xl-5">
+
               <div class="row mb-3">
                 <div class="col-12 col-lg-6 d-flex align-items-center control-label">{{ $t('setup_debit.create.labels.card_prog_code') }}</div>
-                <div class="col-12 col-lg-6"><input disabled="disabled" type="text" :placeholder="$t('setup_debit.create.labels.card_prog_code')" class="form-control"></div>
+                <div class="col-12 col-lg-6">
+									<input v-model="setupDebitData.card_program_code" disabled="disabled" type="text" :placeholder="$t('setup_debit.create.labels.card_prog_code')" class="form-control">
+								</div>
               </div>
               <div class="row mb-3">
                 <div class="col-12 col-lg-6 d-flex align-items-center control-label">{{ $t('setup_debit.create.labels.reseller_code') }}</div>
-                <div class="col-12 col-lg-6"><input disabled="disabled" type="text" :placeholder="$t('setup_debit.create.labels.reseller_code')" class="form-control  form-control-danger"></div>
+                <div class="col-12 col-lg-6">
+									<input v-model="setupDebitData.reseller_code" disabled="disabled" type="text" :placeholder="$t('setup_debit.create.labels.reseller_code')" class="form-control  form-control-danger">
+								</div>
               </div>
               <div class="row mb-3">
                 <div class="col-12 col-lg-6 d-flex align-items-center control-label">{{ $t('setup_debit.create.labels.reseller_name') }}</div>
-                <div class="col-12 col-lg-6"><input disabled="disabled" type="text" :placeholder="$t('setup_debit.create.labels.reseller_name')" class="form-control  form-control-danger"></div>
+                <div class="col-12 col-lg-6">
+									<input v-model="setupDebitData.reseller_name" disabled="disabled" type="text" :placeholder="$t('setup_debit.create.labels.reseller_name')" class="form-control  form-control-danger"></div>
               </div>
               <div class="row mb-3">
                 <div class="col-12 col-lg-6 d-flex align-items-center control-label">{{ $t('setup_debit.create.labels.user') }}</div>
-                <div class="col-12 col-lg-6"><input disabled="disabled" type="text" :placeholder="$t('setup_debit.create.labels.user')" class="form-control  form-control-danger"></div>
+                <div class="col-12 col-lg-6">
+									<input v-model="setupDebitData.user" disabled="disabled" type="text" :placeholder="$t('setup_debit.create.labels.user')" class="form-control  form-control-danger"></div>
               </div>
             </div>
 
             <!-- right content-->
-            <div class="w-50 w-sm-100 pr-4 pl-4 pl-md-3">
-              <div class="row mb-3">
-                <div class="col-12 col-lg-6 d-flex align-items-center control-label">{{ $t('setup_debit.create.labels.entry_type') }} <span class="required-field-sympol">＊</span></div>
+            <div class="w-50 w-sm-100 pr-4 pl-4 pl-md-3 pl-lg-4 pl-xl-5">
+              <div class="row mb-0">
+                <div class="col-12 col-lg-6 d-flex align-items-center control-label mb-lg-3">{{ $t('setup_debit.create.labels.entry_type') }} <span class="required-field-sympol">＊</span></div>
                 <div class="col-12 col-lg-6">
-                  <el-select v-model="selected" :placeholder="$t('setup_debit.create.labels.entry_type')"
+                  <el-select v-model="setupDebitData.entryType" 
+														 v-validate.initial="'required'"
+														 name="entryType"
+														 data-vv-as="charge type"
+														 :placeholder="$t('setup_debit.create.labels.entry_type')"
                              class="w-100">
                     <el-option v-for="(item, index) in entryTypeValues"
                                 :key="index"
@@ -94,45 +105,61 @@
                                 :value="item.value">{{ item.name }}
                     </el-option>
                   </el-select>	
-
+									<div class="validation-error">{{ errors.first('entryType') }}</div>
                 </div>
               </div>
               <div class="row mb-3">
                 <div class="col-12 col-lg-6 d-flex align-items-center control-label">{{ $t('setup_debit.create.labels.currency') }}</div>
-                <div class="col-12 col-lg-6"><input disabled="disabled" type="text" :placeholder="$t('setup_debit.create.labels.currency')" class="form-control  form-control-danger"></div>
+                <div class="col-12 col-lg-6">
+									<input v-model="setupDebitData.currency" disabled="disabled" type="text" :placeholder="$t('setup_debit.create.labels.currency')" class="form-control  form-control-danger"></div>
+              </div>
+              <div class="row mb-0">
+                <div class="col-12 col-lg-6 d-flex align-items-center control-label mb-lg-3">{{ $t('setup_debit.create.labels.entry_desc') }} <span class="required-field-sympol">＊</span></div>
+                <div class="col-12 col-lg-6">
+									<input v-model="setupDebitData.entryDesc"
+ 												 v-validate.initial="'required'"
+												 name="entryDesc"
+												 data-vv-as="description"
+												 type="text" 
+												 :placeholder="$t('setup_debit.create.labels.entry_desc')" 
+												 class="form-control form-control-danger">
+									<div class="validation-error">{{ errors.first('entryDesc') }}</div>
+								</div>									
               </div>
               <div class="row mb-3">
-                <div class="col-12 col-lg-6 d-flex align-items-center control-label">{{ $t('setup_debit.create.labels.entry_desc') }} <span class="required-field-sympol">＊</span></div>
-                <div class="col-12 col-lg-6"><input type="text" :placeholder="$t('setup_debit.create.labels.entry_desc')" class="form-control  form-control-danger"></div>
-              </div>
-              <div class="row mb-3">
-                <div class="col-12 col-lg-6 d-flex align-items-center control-label">{{ $t('setup_debit.create.labels.amount') }} <span class="required-field-sympol">＊</span></div>
-                <div class="col-12 col-lg-6"><input type="text" :placeholder="$t('setup_debit.create.labels.amount')" class="form-control  form-control-danger"></div>
+                <div class="col-12 col-lg-6 d-flex align-items-center control-label mb-lg-3">{{ $t('setup_debit.create.labels.amount') }} <span class="required-field-sympol">＊</span></div>
+                <div class="col-12 col-lg-6">
+									<input v-model="setupDebitData.amount" 
+ 												 v-validate.initial="'required|decimal:2'"
+												 name="amount"
+												 data-vv-as="amount"
+												 type="text" 
+												 :placeholder="$t('setup_debit.create.labels.amount')" 
+												 class="form-control form-control-danger">
+									<div class="validation-error">{{ errors.first('amount') }}</div>
+								</div>
               </div>
             </div>
 
             <div class="ml-auto w-50 w-sm-100 pr-4 pl-4 pl-md-5 pr-md-4">
-              <div class="row">
-                <div class="ml-auto col-12 col-lg-7">
-                <p-button round
-                  class="w-100"
-                  @click="showSoftDocs(index)">
-                  {{ $t('setup_debit.create.button.sof') }}
-                </p-button>
-                </div>
+              <div class="row justify-content-end">
+								<p-button round
+									class="mr-3"
+									@click="showSoftDocs()">
+									{{ $t('setup_debit.create.button.sof') }}
+								</p-button>
               </div>
             </div>
-
           </div>
         </div>
       </div>
     </div>
     <!-- New style mock End-->
 
-    <div class="col-12 mt-5 text-center ceevo__btn-group">
-      <p-button round type="primary" @click="handleSettingDebit"
-                :disabled="!valid"
-      >
+    <div class="col-12 mt-4 text-center ceevo__btn-group">
+      <p-button round type="primary" 
+								@click="handleSettingDebit"
+                :disabled="loading || errors.all().length > 0">
         <div class="d-flex align-items-center">
           <loader v-if="loadingState ==='sending'"></loader>
           {{ $t('setup_debit.create.button.save') }}
@@ -146,7 +173,7 @@
 </template>
 
 <script>
-  import {Table, TableColumn} from 'element-ui'
+  //import {Table, TableColumn} from 'element-ui'
   import Vue from 'vue'
   import {AbaModalEvents} from "../../../../main";
   import {mapActions, mapGetters} from 'vuex'
@@ -160,23 +187,31 @@
     GETTER_LOADINGSTATE_FLOAT_BALANCE, GETTER_RESELLER_SUBSCRIPTIONS, SET_MODAL_TYPE, SHOW_TOAST_MESSAGE,
     GETTER_USER_INFO
   } from '../../../../store/types';
-  import createNewRowFromHeadings from "../../../../utils/createNewRowFromHeadings";
-  import {breakInput, mustBeAValidISOcurrency, shouldBeNumber} from "../../../../utils/formValidations";
+  //import createNewRowFromHeadings from "../../../../utils/createNewRowFromHeadings";
+  //import {breakInput, mustBeAValidISOcurrency, shouldBeNumber} from "../../../../utils/formValidations";
   import {decimals} from "../../../../utils/inputMasks";
   import Uploader from "../../../UIComponents/ABAComponents/abaUploader/Uploader";
   import SofUploader from "../../../UIComponents/ABAComponents/SofUploader/SofUploader";
   import PButton from "../../../UIComponents/Button";
-  import RegularTable from '../../../UIComponents/CeevoTables/RegularTable/RegularTable'
+  //import RegularTable from '../../../UIComponents/CeevoTables/RegularTable/RegularTable'
   import Loader from "../../../UIComponents/Loader";
   import Spinner from "../../../UIComponents/Spinner";
 
-  Vue.use(Table)
-  Vue.use(TableColumn)
+  //Vue.use(Table)
+  //Vue.use(TableColumn)
   export default {
     name: 'SetupDebit',
-    components: {PButton, SofUploader, Uploader, Loader, Spinner, RegularTable},
+    components: {
+			PButton, 
+			SofUploader, 
+			Uploader, 
+			Loader, 
+			Spinner, 
+			//RegularTable
+		},
     data() {
       return {
+				/*
         tableHeadings: [
           {
             label: 'CPC',
@@ -240,7 +275,8 @@
             brakeAt: breakInput(8),
             required: true,
           },
-        ],
+				],
+				*/
         tableData: [],
         editId: '',
         cardProgramCode: '',
@@ -258,9 +294,23 @@
         },
         // 
         entryTypeValues: [
-          { name: 'Credit', value: 'Credit' },
-          { name: 'Debit', value: 'Debit' }
-        ]
+          { name: 'Credit', value: 'CREDIT' },
+          { name: 'Debit', value: 'DEBIT' }
+				],
+				setupDebitData: {
+					card_program_code: '',
+					reseller_code: '',
+					reseller_name: '',
+					user: '',
+					entryType: '',
+					currency: '',
+					entryDesc: '',
+					amount: '',
+					// ???
+					card_program_id: '',
+					reseller_id: ''	
+				},
+				loading: false,
       }
     },
     computed: {
@@ -269,7 +319,7 @@
         loadingState: GETTER_LOADINGSTATE_FLOAT_BALANCE,
       }),
       creationResponseState() { return this.$store.state.UiModule.responseState[ADD_FLOAT_ENTRY]},
-
+			/*
       cardPrograms() {
         return (this.$store.state.cardProgram.allCardPrograms || [])
           .reduce((acc, i) => !!acc.find(({cardProgCode}) => i.cardProgCode === cardProgCode)
@@ -284,16 +334,22 @@
             ? acc
             : [...acc, i]
             , [])
-      }
-    }, watch: {
+			}
+			*/
+		},
+		watch: {
+			/*
       resellerSubscriptions(newVal) {
         this.handleResellerMenuItems(newVal)
-      }, cardPrograms(newVal) {
+			}, 
+			cardPrograms(newVal) {
         this.tableHeadings = this.tableHeadings.map(i => i.name === 'cardProgId' ? {
           ...i, selectKeys: newVal.map(({id, cardProgCode}) =>
             ({name: cardProgCode, value: id}))
         } : i)
-      }, creationResponseState(newVal, oldVal) {
+			},
+			*/ 
+			creationResponseState(newVal, oldVal) {
         if (!oldVal) return this.alert(newVal)
         if (newVal.timeStamp === oldVal.timeStamp) return;
         this.alert(newVal)
@@ -315,14 +371,17 @@
         getAllResellerSubscription: GET_ALL_RESELLER_SUBSCRIPTIONS,
         setUpDebit: ADD_FLOAT_ENTRY,
         showModal: SET_MODAL_TYPE
-      }), showSoftDocs({index: {id}}) {
-        this.selectedFloatId = id;
-        this.showSoftUploader = true;
-      }, handleSofDocsSelection(ev) {
+			}), 
+			showSoftDocs () {
+        this.selectedFloatId = 'setup_debit_new_row'
+        this.showSoftUploader = true
+			}, 
+			handleSofDocsSelection(ev) {
         this.softDocs = ev.map(({data: content, type: mimeType}) => ({
           content, mimeType
         }))
-      }, alert(newVal) {
+			}, 
+			alert(newVal) {
         if (newVal.state === true) {
           const key = 'handleresponsestate' + 'createreseller';
 
@@ -344,7 +403,8 @@
             AbaModalEvents.$off(key)
           })
         }
-      },
+			},
+			/*
       handleResellerMenuItems(newVal) {
         console.log(newVal, '');
         this.tableHeadings = this.tableHeadings.map(i => i.name === 'resellerId' ? {
@@ -356,15 +416,9 @@
             ({name: resellerCode, value: resellerId}))
         } : i)
 
-      },
+			},
       handleInput({value, valid, dirty}) {
         this.tableData = value;
-        /*
-             const resellerId = this.tableData[0].resellerId;
-             const selectKey = this.tableHeadings.find(i => i.name === 'resellerId')['selectKeys']
-             this.tableData[0].resellerId = selectKey.find(({value}) => value === resellerId) ?
-               resellerId && this.tableData[0].resellerId
-               : selectKey.length === 0 ? resellerId && this.tableData[0].resellerId : ''*/
         this.valid = valid;
         this.dirty = this.dirty || !!dirty;
         const {
@@ -377,7 +431,9 @@
           ...dataToSend
         } = value[0];
         this.dataToSend = dataToSend
-      }, handleSecondaryAction() {
+			},
+			*/ 
+			handleSecondaryAction() {
         if (!this.dirty) return this.$router.push('/float-account/view-float-account')
         const key = 'handle_secodary_aciton_set_up_debit';
         this.showModal({
@@ -391,15 +447,14 @@
         AbaModalEvents.$on(key, response => {
           if (response.ok) {
             this.$router.push('/float-account/view-float-account')
-
           }
           AbaModalEvents.$off(key)
-
         })
       },
       handleSettingDebit() {
         // TODO fix it here
-        const key = 'handle_main_aciton_set_up_debit';
+				const key = 'handle_main_aciton_set_up_debit'
+				
         this.showModal({
           type: 'normal',
           message: `Set up a new ${this.dataToSend.entryType} ?`,
@@ -407,22 +462,31 @@
           mainButton: 'Ok',
           secondaryButton: 'No',
           key
-        })
+				})
+				
         AbaModalEvents.$on(key, response => {
           if (response.ok) {
-
-
-            const {cardProgId, ...body} = this.dataToSend;
-            console.log(body);
-            if (this.loadingState === 'sending') return;
-            if (!this.valid) return this.$store.dispatch(SHOW_TOAST_MESSAGE, {message: 'Please ensure you complete all fields correctly.', status: 'danger'})
-            console.log(this.softDocs, 'sof docs doc');
-            this.setUpDebit({id: cardProgId, body, sofDocs: this.softDocs})
-          }
+						let body = {	
+							resellerId: this.setupDebitData.resellerId,
+							entryType: this.setupDebitData.entryType,
+							currency: this.setupDebitData.currency,
+							entryDesc: this.setupDebitData.entryDesc,
+							amount: this.setupDebitData.amount
+						}
+            //const {cardProgId, ...body} = this.dataToSend;
+						//console.log('Sending body', body);
+						
+						//if (this.loadingState === 'sending') return;						
+            //if (!this.valid) return this.$store.dispatch(SHOW_TOAST_MESSAGE, {message: 'Please ensure you complete all fields correctly.', status: 'danger'})
+						//console.log('sof docs doc', this.softDocs)
+						
+            this.setUpDebit({ id: this.setupDebitData.cardProgId, body, sofDocs: this.softDocs })
+					}
+					
           AbaModalEvents.$off(key)
-
         })
-      }, handleChoseFile(file, id) {
+			}, 
+			handleChoseFile(file, id) {
         console.log(file);
         this.files = {
           ...this.files,
@@ -430,11 +494,29 @@
         }
       }
     },
-    mounted() {
+    mounted () {
+			// New data load
+			this.setupDebitData = {
+				entryType: '',
+				entryDesc: '',
+				amount: ''
+			}
+
+			this.setupDebitData.card_program_code = this.$route.query.card_program_code
+			this.setupDebitData.reseller_code = this.$route.query.reseller_code
+			this.setupDebitData.reseller_name = this.$route.query.reseller_name
+			this.setupDebitData.resellerId = this.$route.query.reseller_id // not used in form but send to server
+			this.setupDebitData.cardProgId = this.$route.query.card_program_id // not used in form but send to server
+			this.setupDebitData.user = this.userInfo.preferred_username  || ''
+			this.setupDebitData.currency = this.$route.query.currency
+			
+			// Old
+			/*
       console.log(this.$route.query);
       this.getAllResellerSubscription();
       this.getAllCards();
-      this.editId = 'setup_debit_new_row'
+			this.editId = 'setup_debit_new_row'
+			
       let {
         card_program_code,
         reseller_code,
@@ -443,7 +525,7 @@
         user,
         card_program_id,
         reseller_id,
-      } = this.$route.query;
+			} = this.$route.query
 
       this.tableData = [{
         ...createNewRowFromHeadings(this.tableHeadings, 'setup_debit_new_row'),
@@ -454,7 +536,8 @@
         resellerId: reseller_id,
         cardProgId: card_program_id,
         user: this.userInfo.preferred_username  || ''
-      }]
+			}]
+			*/
     }
   }
 </script>
